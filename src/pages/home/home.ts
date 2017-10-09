b369556752e1fe1239216d807af70d06980eaf15
unknown <samarthagarwal@live.com>
2017-10-09 20:18:01 +0530

    AutoCollapse

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { AccordionComponent } from '../../components/accordion/accordion';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: any[] = [];
 
+  constructor(public navCtrl: NavController, private http: Http) {
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((data) => {
+
+      this.users = data.json();
+
+      this.users.forEach(u => u.accordionExpanded = false);
+
+      console.log(this.users);
+    })
+  }
+
+  onClickAccordion(user){
+    console.log(user.accordionExpanded)
+    let backup = user.accordionExpanded;
+    this.users.forEach(u => u.accordionExpanded = false);
+    user.accordionExpanded = !backup;
   }
 
 }
